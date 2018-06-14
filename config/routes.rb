1a57da5a5a674c7316e2030758a05fafdebb79a3
Lisa Lee <lisa_lee@alumni.brown.edu>
2018-06-14 18:50:40 -0400

    added session controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
     namespace :v1 do 
       resources :users
       resources :lists
-      resources :items 
+      resources :items
+      post "/login", to: "sessions#create"
+      get "/current_user", to: "sessions#curr_user"
+      post "/signup", to: "user#new"
+      delete "/logout", to: "sessions#destroy"
     end 
   end 
 end
